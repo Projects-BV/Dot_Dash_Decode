27a5ea0e0b2f6469b1566efd2d73e3de7ba250c5
srivastava811 <srivastavatulika0811@gmail.com>
2025-03-28 08:39:04 +0530

    minor changes

--- Frontend/react-flask-app/src/api.js
+++ Frontend/react-flask-app/src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 //flask backend URL
 //const API_URL="https://127.0.0.1:5000/auth";
+//s not added because not secured
 const API_URL="http://localhost:5000/auth";
 
 //setup axios default for ease
