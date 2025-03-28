8bee8b51735beaa866d54bc24dff95a654b56d50
srivastava811 <srivastavatulika0811@gmail.com>
2025-03-28 08:42:04 +0530

    minor changes

--- Backend/flask/db_setup.py
+++ Backend/flask/db_setup.py
@@ -1,7 +1,4 @@
-
-
-
-
+#importing dependencies
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_bcrypt import Bcrypt
@@ -9,6 +6,7 @@ from flask_bcrypt import Bcrypt
 db=SQLAlchemy()
 bcrypt=Bcrypt()
 
+#create user model
 class User(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     username=db.Column(db.String(80),unique=True,nullable=False)
